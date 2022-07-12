5c3c213dcf4942e195b917fcf8375c738c9a7bcd
mksmp <panin.ma2013u@mail.ru>
2022-07-13 01:25:47 +0300

    task

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from fake_useragent import UserAgent
 
 url_pochta = "https://www.pochtabank.ru/"
 url_fin = "https://finambank.ru/"
-url_bross = "https://www.uralsib.ru/"
+url_urlsb = "https://www.uralsib.ru/"
 url_expo = "https://expobank.ru/"
 url_vtb = "https://www.vtb.ru/"
 
@@ -30,9 +30,6 @@ while True:
         request_pochta = None
     except requests.exceptions.ProxyError:
         print('Не удалось подключиться к proxy. Переподключение')
-        # index_proxy = random.randint(0, len(proxy_arr) - 1) # Рандомный индекс прокси
-        # proxies = {'http': proxy_arr[index_proxy], 'https': proxy_arr[index_proxy]}
-        # request_pochta = requests.get(url_pochta, proxies=proxies, timeout=10).status_code
         request_pochta = None
     except:
         print('Что-то пошло не так')
@@ -45,28 +42,22 @@ while True:
         request_fin = None
     except requests.exceptions.ProxyError:
         print('Не удалось подключиться к proxy. Переподключение')
-        # index_proxy = random.randint(0, len(proxy_arr) - 1) # Рандомный индекс прокси
-        # proxies = {'http': proxy_arr[index_proxy], 'https': proxy_arr[index_proxy]}
-        # request_fin = requests.get(url_fin, proxies=proxies, headers=headers, timeout=10).status_code
         request_fin = None    
     except:
         print('Что-то пошло не так')
         request_fin = None        
 
     try:
-        request_bross = requests.get(url_bross, proxies=proxies, headers=headers, timeout=10).status_code
+        request_urlsb = requests.get(url_urlsb, proxies=proxies, headers=headers, timeout=10).status_code
     except requests.exceptions.ConnectTimeout or requests.exceptions.ReadTimeout:
         print('Банк России не отвечает больше 10 секунд')
-        request_bross = None
+        request_urlsb = None
     except requests.exceptions.ProxyError:
         print('Не удалось подключиться к proxy. Переподключение')
-        # index_proxy = random.randint(0, len(proxy_arr) - 1) # Рандомный индекс прокси
-        # proxies = {'http': proxy_arr[index_proxy], 'https': proxy_arr[index_proxy]}
-        # request_bross = requests.get(url_bross, proxies=proxies, headers=headers, timeout=10).status_code
-        request_bross = None
+        request_urlsb = None
     except:
         print('Что-то пошло не так')
-        request_bross = None        
+        request_urlsb = None        
 
     try:
         request_expo = requests.get(url_expo, proxies=proxies, headers=headers, timeout=10).status_code
@@ -75,9 +66,6 @@ while True:
         request_expo = None
     except requests.exceptions.ProxyError:
         print('Не удалось подключиться к proxy. Переподключение')
-        # index_proxy = random.randint(0, len(proxy_arr) - 1) # Рандомный индекс прокси
-        # proxies = {'http': proxy_arr[index_proxy], 'https': proxy_arr[index_proxy]}
-        # request_expo = requests.get(url_expo, proxies=proxies, headers=headers, timeout=10).status_code
         request_expo = None
     except:
         print('Что-то пошло не так')
@@ -90,9 +78,6 @@ while True:
         request_vtb = None
     except requests.exceptions.ProxyError:
         print('Не удалось подключиться к proxy. Переподключение')
-        # index_proxy = random.randint(0, len(proxy_arr) - 1) # Рандомный индекс прокси
-        # proxies = {'http': proxy_arr[index_proxy], 'https': proxy_arr[index_proxy]}
-        # request_vtb = requests.get(url_vtb, proxies=proxies, headers=headers, timeout=10).status_code
         request_vtb = None
     except:
         print('Что-то пошло не так')
@@ -103,8 +88,8 @@ while True:
         print(now, request_pochta, "Почта Банк")
     if request_fin is None or int(request_fin) != 200:
         print(now, request_fin, "Финам Банк")
-    if request_bross is None or int(request_bross) != 200:
-        print(now, request_bross, "Банк России")
+    if request_urlsb is None or int(request_urlsb) != 200:
+        print(now, request_urlsb, "Банк России")
     if request_expo is None or int(request_expo) != 200:
         print(now, request_expo, "Экспо банк")
     if request_vtb is None or int(request_vtb) != 200:
