01762ac9f94a52f05fed8609546b4581a00e0f5a
mksmp <panin.ma2013u@mail.ru>
2022-07-13 01:12:45 +0300

    task

---
+++ proxy.py
@@ -0,0 +1,6 @@
+proxies = ['http://fYjQrd:BZMX8w@91.193.111.86:8000/', 'http://JLy4kU:zXY1od@45.129.7.41:8000/',
+           'http://Rrx1u4:prJa5G@193.56.188.102:8000/', 'http://TrdXEX:vQLTh8@45.89.73.164:8000/', 
+           'http://kf8u7a:3p8Rns@45.9.42.153:8000/', 'http://ACRujN:S4Z0Dv@45.139.169.161:8000/'
+        ]
+
+# 'http://XYXuZV:ZZQzuK@217.197.175.204:8000/'
